9f734c2f02832e1a8edce76662b6ecc71de169aa
yonna-laplateforme <yonna.merlini@laplateforme.io>
2026-06-29 16:35:56 +0200

    fix front

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://portfolio-backend-7xj4.onrender.com',
+        changeOrigin: true,
+        secure: false, // Utile si Render a des soucis de certificat
+      }
+    }
+  }
 });
